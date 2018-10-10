2083c9a5f55a2f4f76cb6b16f54cc1ca058277ce
Erenv5 <997073909@qq.com>
2018-10-10 20:10:46 +0800

    先排序后比较中，使用了自带排序方法，等到看排序时要回来看

--- AOP-July/src/stringContain/BroString.java
+++ AOP-July/src/stringContain/BroString.java
@@ -1,5 +1,7 @@
 package stringContain;
 
+import java.util.Arrays;
+
 /**
  * 兄弟字符串
  * 如果两个字符串的字符一样，但是顺序不一样，被认为是兄弟字符串，
@@ -16,30 +18,29 @@ package stringContain;
 public class BroString {
 
     /**
-     * Stringbuilder 快速排序 元操作
-     * @param a
+     * 自带排序
+     * @param S
      * @return StringBuilder
      */
-    private static StringBuilder sortString (StringBuilder a) {
-        int i,j;
-
-        for(i=0; i<a.length(); ) {
+    private static void sortString (String S) {
+        int i=0,j;
+        StringBuilder a = new StringBuilder(S);
             for(j=1; j<a.length(); j++) {
-                if(a.charAt(i) >= a.charAt(j)) {
+                if(a.charAt(i) > a.charAt(j)) {
                     char b = a.charAt(i);
                     a.setCharAt(i,a.charAt(j));
                     a.setCharAt(j,b);
                     i = j;
                 }
-            }
-        }
-        return a;
+             }
+
+
     }
 
 
     /**
      * 1. 从 B 串中一个一个拿出来在A中比较
-     * 缺点： 很难判断重复字符
+     * 缺点： 不能有重复字符
      * @param a
      * @param b
      * @return
@@ -66,12 +67,27 @@ public class BroString {
      * @return
      */
     private static boolean afterSort (String a, String b) {
-        StringBuilder sa = new StringBuilder(a);
-        StringBuilder sb = new StringBuilder(b);
-        return false;
+        int length = 0;
+        if(a.length() != b.length())
+            return false;
+
+        for(int i=0; i<a.length(); i++) {
+            if(a.charAt(i) != b.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(oneByOne("sdaa","dsae"));
+        String s = "abvnmiuy";
+        String b = "abnmuiyyv";
+        char[] S = s.toCharArray();
+        char[] B = b.toCharArray();
+        Arrays.sort(B);
+        Arrays.sort(S);
+        System.out.println(new String(S));
+        System.out.println(new String(B));
+        System.out.println(afterSort(new String(S), new String(B)));
     }
 }
