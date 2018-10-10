ec8bb7d5f7681bebba432ab197058d47034b7f7e
Erenv5 <997073909@qq.com>
2018-10-10 20:11:18 +0800

    先排序后比较中，使用了自带排序方法，等到看排序时要回来看

---
+++ AOP-July/src/stringToInt/StringToInt.java
@@ -0,0 +1,18 @@
+package stringToInt;
+
+public class StringToInt {
+
+    static int StrToInt(String str){
+
+
+        return 1;
+    }
+
+
+
+    public static void main(String[] args) {
+        String s = "a";
+        int i = Integer.parseInt(s);
+        System.out.println(i);
+    }
+}
